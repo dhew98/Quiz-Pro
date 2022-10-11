7da72058975b8bdf1247b4d682324eef0ee3538d
Jawad Dhew <jawaddhew98@gmail.com>
2022-10-11 15:28:17 +0600

    showing qstn

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,19 +2,35 @@ import React from 'react';
 import './Quiz.css'
 
 const Quiz = ({ quiz }) => {
-    const { question, option } = quiz;
+    const { question, options } = quiz;
+    console.log(options)
     return (
-        <div>
-            <div className="card card-container" >
-                <div className="card-header">
-                    {question}
-                </div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">An item</li>
-                    <li className="list-group-item">A second item</li>
-                    <li className="list-group-item">A third item</li>
+        <div className='card-container'>
+            <ul class="list-group">
+                <li class="list-group-item active" aria-current="true">{question}</li>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." />
+                        First checkbox
+                    </li>
+                    <li class="list-group-item">
+                        <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." />
+                        Second checkbox
+                    </li>
+                    <li class="list-group-item">
+                        <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." />
+                        Third checkbox
+                    </li>
+                    <li class="list-group-item">
+                        <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." />
+                        Fourth checkbox
+                    </li>
+                    <li class="list-group-item">
+                        <input class="form-check-input me-1" type="checkbox" value="" aria-label="..." />
+                        Fifth checkbox
+                    </li>
                 </ul>
-            </div>
+            </ul>
         </div>
     );
 };
