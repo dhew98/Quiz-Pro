37e0802a997aca336ea55ee182b773fc2d54019f
Jawad Dhew <jawaddhew98@gmail.com>
2022-10-11 21:36:35 +0600

    chart done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import './Statistics.css'
 
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
@@ -12,17 +13,17 @@ const Statistics = () => {
 
 
     return (
-        <div>
+        <div >
             <h1>Chart</h1>
-            <ResponsiveContainer width="100%" height="100%">
-                <LineChart width={600} height={300} data={topics} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                    <Line type="monotone" dataKey="total" stroke="#8884d8" />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                </LineChart>
-            </ResponsiveContainer>
+
+            <LineChart width={600} height={300} data={topics} className='chart'>
+                <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+            </LineChart>
+
         </div>
     );
 };
