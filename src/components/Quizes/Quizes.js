76014abf1ff3e45741a8bef77d605c040e7617fb
Jawad Dhew <jawaddhew98@gmail.com>
2022-10-12 17:24:12 +0600

    quiz head

--- src/components/Quizes/Quizes.js
+++ src/components/Quizes/Quizes.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
 import './Quizes.css'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 
 const Quizes = () => {
     const data = useLoaderData();
+    const { name, total } = data.data;
     const quizes = data.data.questions;
     console.log(quizes)
     return (
         <div>
             <ToastContainer></ToastContainer>
+
+
             <div className="quiz-container ">
+                <div>
+                    <h1> Quiz of {name}</h1>
+                    <h4>Total quiz : {total}</h4>
+                </div>
+
                 {
                     quizes.map(quiz =>
                         <Quiz key={quiz.id} quiz={quiz}></Quiz>
