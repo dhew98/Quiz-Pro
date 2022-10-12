b73d2b2adea322c06cc8fad9437f719bc3f465ca
Jawad Dhew <jawaddhew98@gmail.com>
2022-10-12 06:27:48 +0600

    chart responsive

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -7,7 +7,7 @@ const Options = ({ option }) => {
         <div className='options'>
             <li className="list-group-item fw-semibold ">
 
-                <input className="form-check-input me-1" type="checkbox" value="" id="firstCheckbox" />
+                <input className="form-check-input me-1" type="radio" name="firstradio" value="" id="firstCheckbox" />
                 <label className="form-check-label " for="firstCheckbox">{option}</label>
             </li>
 
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -15,14 +15,15 @@ const Statistics = () => {
     return (
         <div >
             <h1>Chart</h1>
-
-            <LineChart width={600} height={300} data={topics} className='chart'>
-                <Line type="monotone" dataKey="total" stroke="#8884d8" />
-                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-            </LineChart>
+            <ResponsiveContainer width="50%" height={350} className='chart'>
+                <LineChart data={topics} >
+                    <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                </LineChart>
+            </ResponsiveContainer>
 
         </div>
     );
